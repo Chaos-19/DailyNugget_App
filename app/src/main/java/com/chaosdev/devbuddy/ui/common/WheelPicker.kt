cf1f791f284bcc189bbebe3ff885cc9aa3a21904
Chaos-19 <kalgetachew375@gmail.com>
2025-07-09 14:26:02 +0300

    fix:update the onboarding screen
    add a topbaron onboarding screen

--- app/src/main/java/com/chaosdev/devbuddy/ui/common/WheelPicker.kt
+++ app/src/main/java/com/chaosdev/devbuddy/ui/common/WheelPicker.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
